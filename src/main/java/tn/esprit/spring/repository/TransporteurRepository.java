cf978435039e3d272a206c707fef488a2ab4cc8d
ShaymaRebhi <79281085+ShaymaRebhi@users.noreply.github.com>
2021-10-26 23:42:09 +0100

    CrudTransporteur

---
+++ src/main/java/tn/esprit/spring/repository/TransporteurRepository.java
@@ -0,0 +1,10 @@
+package tn.esprit.spring.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import tn.esprit.spring.entities.Transporteur;
+
+public interface TransporteurRepository extends CrudRepository<Transporteur , Long >{
+	 
+
+}
