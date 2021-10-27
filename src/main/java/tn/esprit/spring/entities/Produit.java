edd0b7ededdff006d83dc80c0c7a6a0d91a78b61
OueslatiHassen <hassen.oueslati@esprit.tn>
2021-10-27 12:02:51 +0100

    Update Produit.java

--- src/main/java/tn/esprit/spring/entities/Produit.java
+++ src/main/java/tn/esprit/spring/entities/Produit.java
@@ -30,15 +30,13 @@ public class Produit implements Serializable {
 		private Set<Fournisseur> Produit;
 		@ManyToMany(cascade = CascadeType.ALL)
 		private Set<Transporteur> ProduitT;
-		
-		
-		
 		@ManyToOne
 		Rayon rayon;
 		@ManyToOne
 		Stock stock;
 		@ManyToOne
 		DetailFacture detailFacture;
+		
 		
 		public Long getIdProduit() {
 			return idProduit;
