c472018952a29f2b006c88630d5f5d4873d73f56
badisraissi <49197543+badisraissi@users.noreply.github.com>
2021-11-30 09:52:04 +0100

    update

--- src/main/java/tn/esprit/spring/configuration/SwaggerConfig.java
+++ src/main/java/tn/esprit/spring/configuration/SwaggerConfig.java
@@ -2,7 +2,6 @@ package tn.esprit.spring.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -15,17 +14,17 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class SwaggerConfig {
 	@Bean
     public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2).select()
-        		.apis(RequestHandlerSelectors
-        		.basePackage("tn.esprit.spring.controller"))
-        		.paths(PathSelectors.any()).build();
+    return new Docket(DocumentationType.SWAGGER_2)
+    .select()
+    .apis(RequestHandlerSelectors.any())
+    .paths(PathSelectors.any())
+    .build().apiInfo(apiInfo())
+    ;
     }
-
 	private ApiInfo apiInfo () {
 		return new ApiInfoBuilder()
 		.title("Swagger Configuration for tpStockProject")
 		.description("\"Spring Boot Swagger configuration\"")
 		.version("1.1.0").build();
 		}
-
 }
