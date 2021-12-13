ee7db75437f9f7c59d327262f53b64bf2cff8b58
ShaymaREBHI <shayma.rebhi@esprit.tn>
2021-12-14 00:15:57 +0100

    update

--- src/main/java/tn/esprit/spring/entities/Rayon.java
+++ src/main/java/tn/esprit/spring/entities/Rayon.java
@@ -5,14 +5,7 @@ import lombok.*;
 import java.io.Serializable;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -34,6 +27,8 @@ public class Rayon  implements Serializable {
 	private String image;
 	@OneToMany(cascade = CascadeType.ALL, mappedBy="rayon")
 	private Set<Produit> Produit;
+	@ManyToOne
+	Departement departement;
 
 
 
